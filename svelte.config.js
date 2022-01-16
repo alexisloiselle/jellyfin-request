7b483dbd31a4f21d975c21fe073001137ba99b98
Alexis Loiselle <alexis@t-b.ca>
2022-01-16 17:18:53 -0500

    fix paths and _app

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const production = process.env.NODE_ENV !== 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -11,12 +9,7 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-		// Comment the paths if wants to run in dev mode.
-		paths: production
-			? {
-					base: '/jellyfin-request'
-			  }
-			: undefined,
+
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
