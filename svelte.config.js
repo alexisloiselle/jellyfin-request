8924edb63bf97ef0345d155f300b21f4918ad8d0
Alexis Loiselle <alexis@t-b.ca>
2022-01-16 17:45:30 -0500

    patch svelte kit cli for allowing ./_app in assets

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const production = process.env.NODE_ENV !== 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -9,6 +11,11 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+		paths: production
+			? {
+					assets: '.'
+			  }
+			: undefined,
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
